1e00ca2f9f06377878a077902d28b25f967799de
kachanovdaniil <kachanodanilka@gmail.com>
2023-10-24 20:39:55 +0300

    Web: lab1 float validation fixed

--- Веб/lab1/src/script_validation.js
+++ Веб/lab1/src/script_validation.js
@@ -1,8 +1,19 @@
 
+function checkAfterPointLength(num){
+   const regex = /^\d+\.\d{15}/;
+    return !regex.test(num.toString());
+}
+
 export function validateData(R, X, Y) {
-    const isRValid = !isNaN(R) && R > 2 && R < 5; 
-    const isXValid = !isNaN(X) && X >= -3 && X <= 5;
-    const isYValid = !isNaN(Y) && Y > -3 && Y < 3;
+    console.log(X);
+    console.log(Y);
+    console.log(R);
+    const isRValid = !isNaN(R) && R > 2 && R < 5 && checkAfterPointLength(R); 
+    const isXValid = !isNaN(X) && X >= -3 && X <= 5 && checkAfterPointLength(X); 
+    const isYValid = !isNaN(Y) && Y > -3 && Y < 3 && checkAfterPointLength(Y); 
+    console.log(isRValid);
+    console.log(isYValid);
+    console.log(isXValid);
   
 if (!isRValid) {
     document.getElementById('errorR').style.display = 'block'; // Показать сообщение об ошибке для R
